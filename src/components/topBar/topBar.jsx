bc9845bc152b3624f268b2b540aa0b834070cefc
Ben Terry <beterry12@gmail.com>
2020-07-28 11:55:42 -0400

    drawer styles complete

--- src/components/topBar/topBar.jsx
+++ src/components/topBar/topBar.jsx
@@ -12,11 +12,7 @@ const NavLink = ({label, link}) => (
 
 const DropdownMenu = ({label, link, children}) => (
     <div className={styles.dropdown}>
-        <a
-            href={link}
-        >
-            {label}
-        </a>
+        <a href={link}>{label}</a>
         <MdArrowDropDown size='1.5rem'/>
         <div className={styles.dropdown_content}>
             {children}
@@ -47,6 +43,67 @@ const TopLinks = () => (
     </div>
 )
 
+class DrawerDropdown extends Component{
+    constructor(props){
+        super(props)
+        this.state = {isOpen: false}
+        this.toggleOpen = this.toggleOpen.bind(this)
+    }
+
+    toggleOpen() {
+        this.setState({isOpen: !this.state.isOpen})
+    }
+
+    render() {
+        return(
+            <>
+            <div className={styles.drawer_link}>
+                <a href={this.props.link}>{this.props.label}</a>
+                <button className='button_icon' onClick={this.toggleOpen}>
+                    <MdArrowDropDown size='1.5rem'/>
+                </button>
+            </div>
+            <div className={styles.drawer_dropdown}>
+                {this.state.isOpen ? 
+                    this.props.children :
+                    null
+                }
+            </div>
+            </>
+        )
+    }
+}
+
+const DrawerLink = ({label, link}) => (
+    <div className={styles.drawer_link}>
+        <a href={link}>{label}</a>
+    </div>
+    
+) 
+
+const DrawerLinks = () => (
+    <div className={styles.drawer_links}>
+        <DrawerLink label='About Us' link='https://www.themailshark.com/about/' />
+        <DrawerDropdown label='Products & Pricing' link='https://www.themailshark.com/products/'>
+            <NavLink label='Postcards' link='https://www.themailshark.com/products/postcards/' />
+            <NavLink label='Postcard Magnets' link='https://www.themailshark.com/products/postcard-magnets/' />
+            <NavLink label='Scratch-Off Postcards' link='https://www.themailshark.com/products/scratch-off-postcards/' />
+            <NavLink label='Plastic Postcards' link='https://www.themailshark.com/products/plastic-postcards/' />
+            <NavLink label='Menus' link='https://www.themailshark.com/products/menus/' />
+            <NavLink label='Brochures' link='https://www.themailshark.com/products/brochures/' />
+            <NavLink label='Letters' link='https://www.themailshark.com/products/letters/' />
+        </DrawerDropdown>
+        <DrawerDropdown label='Direct Mail Services' link='https://www.themailshark.com/direct-mailing-services/'>
+            <NavLink label='Our Strategy' link='https://www.themailshark.com/our-strategy/' />
+            <NavLink label='Mailing Lists' link='https://www.themailshark.com/direct-mailing-services/mailing-lists/' />
+            <NavLink label='Every Door Direct Mail' link='https://www.themailshark.com/direct-mailing-services/every-door-direct-mail/' />
+            <NavLink label='Design' link='https://www.themailshark.com/design-services/' />
+            <NavLink label='Print' link='https://www.themailshark.com/printing-services/' />
+        </DrawerDropdown>
+        <DrawerLink label='Resources' link='https://www.themailshark.com/resources/' />
+    </div>
+)
+
 const Underlay = ({handleClick}) => (
     <button className={styles.underlay} onClick={handleClick}/>
 )
@@ -56,32 +113,10 @@ const NavDrawer = ({isOpen}) => (
         <div className={styles.drawer_logo}>
             <img src={logo} alt='Mail Shark logo' />
         </div>
-        <div className={styles.drawer_links}>
-            <NavLink label='About Us' link='https://www.themailshark.com/about/' />
-        </div>
-        <div className={styles.drawer_links}>
-            <NavLink label='Products & Pricing' link='https://www.themailshark.com/products/' />
-            <NavLink label='Postcards' link='https://www.themailshark.com/products/postcards/' />
-            <NavLink label='Postcard Magnets' link='https://www.themailshark.com/products/postcard-magnets/' />
-            <NavLink label='Scratch-Off Postcards' link='https://www.themailshark.com/products/scratch-off-postcards/' />
-            <NavLink label='Plastic Postcards' link='https://www.themailshark.com/products/plastic-postcards/' />
-            <NavLink label='Menus' link='https://www.themailshark.com/products/menus/' />
-            <NavLink label='Brochures' link='https://www.themailshark.com/products/brochures/' />
-            <NavLink label='Letters' link='https://www.themailshark.com/products/letters/' />
-        </div>
-        <div className={styles.drawer_links}>
-            <NavLink label='Direct Mail Services' link='https://www.themailshark.com/direct-mailing-services/' />
-            <NavLink label='Our Strategy' link='https://www.themailshark.com/our-strategy/' />
-            <NavLink label='Mailing Lists' link='https://www.themailshark.com/direct-mailing-services/mailing-lists/' />
-            <NavLink label='Every Door Direct Mail' link='https://www.themailshark.com/direct-mailing-services/every-door-direct-mail/' />
-            <NavLink label='Design' link='https://www.themailshark.com/design-services/' />
-            <NavLink label='Print' link='https://www.themailshark.com/printing-services/' />
-        </div>
-        <div className={styles.drawer_links}>
-            <NavLink label='Resources' link='https://www.themailshark.com/resources/' />
-        </div>
+        <DrawerLinks />
         <div className={styles.drawer_login}>
             <a
+                className='button_text'
                 href='https://www.themailshark.net/logon.aspx?ReturnUrl=%2f'
                 target='_blank'
                 rel="noopener noreferrer"
@@ -96,7 +131,7 @@ const NavDrawer = ({isOpen}) => (
 export default class topBar extends Component {
     constructor(props){
         super(props)
-        this.state = {drawerOpen: false}
+        this.state = {drawerOpen: true}
         this.toggleDrawer = this.toggleDrawer.bind(this)
     }
 
@@ -121,6 +156,7 @@ export default class topBar extends Component {
                         </div>
                         <div className={styles.right}>
                             <a
+                                className='button_text'
                                 href='https://www.themailshark.net/logon.aspx?ReturnUrl=%2f'
                                 target='_blank'
                                 rel="noopener noreferrer"
