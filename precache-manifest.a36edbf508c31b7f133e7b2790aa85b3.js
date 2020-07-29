fbbc3aec82eab49076189a74c008387f4e418918
Ben Terry <beterry12@gmail.com>
2020-07-29 10:57:17 -0400

    Updates

---
+++ precache-manifest.a36edbf508c31b7f133e7b2790aa85b3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26a3b8c5f0dfee19f819765b8bf4287",
+    "url": "/roi-calc/index.html"
+  },
+  {
+    "revision": "01d9915e9218736d7515",
+    "url": "/roi-calc/static/css/main.1be3ace7.chunk.css"
+  },
+  {
+    "revision": "6ca10aeb3dd36984c395",
+    "url": "/roi-calc/static/js/2.6a872b8a.chunk.js"
+  },
+  {
+    "revision": "c5757f2863d89aa08d26280ee8728033",
+    "url": "/roi-calc/static/js/2.6a872b8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01d9915e9218736d7515",
+    "url": "/roi-calc/static/js/main.c2e0713d.chunk.js"
+  },
+  {
+    "revision": "98f70fc1127a9588548c",
+    "url": "/roi-calc/static/js/runtime-main.e6be1827.js"
+  },
+  {
+    "revision": "8643f3877458f9ae6b502f6c957be84b",
+    "url": "/roi-calc/static/media/calc.8643f387.svg"
+  },
+  {
+    "revision": "75455e395195c57c00acb15d9c83b8e7",
+    "url": "/roi-calc/static/media/fb.75455e39.svg"
+  },
+  {
+    "revision": "cd74d5b0bea59cf16f5d766dd62b1c3e",
+    "url": "/roi-calc/static/media/insta.cd74d5b0.svg"
+  },
+  {
+    "revision": "8a82317bd6fb02fcc689fba864943c42",
+    "url": "/roi-calc/static/media/logo.8a82317b.svg"
+  },
+  {
+    "revision": "2b5cad9f9eac10dd6f0a917f356845c2",
+    "url": "/roi-calc/static/media/wave.2b5cad9f.svg"
+  }
+]);
