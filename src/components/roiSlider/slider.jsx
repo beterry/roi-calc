9c9632c58a95f4d7b3de09581217eede15342ec2
Ben Terry <beterry12@gmail.com>
2020-07-29 10:51:20 -0400

    revamp slider styles

--- src/components/roiSlider/slider.jsx
+++ src/components/roiSlider/slider.jsx
@@ -12,22 +12,31 @@ export default function slider({
     value,
     onChange
 }) {
+
+    const railWidthBefore = `${((value / (max-min)) - (min / (max-min))) * 100}%`
+    const railWidthAfter = `${(1-((value / (max-min)) - (min / (max-min)))) * 100}%`
+
     return (
         <div className={styles.slider}>
             <div className={styles.labels}>
                 <label htmlFor={label}>{label}</label>
                 <label htmlFor={label}>{numeral(value).format(format)}</label>
             </div>
-            <input
-                type='range'
-                id={label}
-                name={label}
-                min={min}
-                max={max}
-                step={step}
-                value={value}
-                onChange={(e) => onChange(e)}
-            />
+            <div className={styles.slider_wrapper}>
+                <div className={styles.rail_before} style={{width: railWidthBefore}} />
+                <div className={styles.rail_after} style={{width: railWidthAfter}} />
+                <input
+                    type='range'
+                    id={label}
+                    name={label}
+                    min={min}
+                    max={max}
+                    step={step}
+                    value={value}
+                    onChange={(e) => onChange(e)}
+                />
+                
+            </div>
             <div className={styles.range}>
                 <p className='caption'>{numeral(min).format(format)}</p>
                 <p className='caption'>{numeral(max).format(format)}</p>
